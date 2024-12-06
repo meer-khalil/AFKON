07181a540b98db27c092d7f0fccb609f38514e4d
meer-khalil <arzishjani1613@gmail.com>
2024-12-06 20:05:05 +0500

    remove api keys

--- config/api.js
+++ config/api.js
@@ -2,10 +2,14 @@ import axios from 'axios';
 
 // Create an Axios instance with default configuration
 const apiClient = axios.create({
-  baseURL: `https://${process.env.FOOTBALL_API_HOST}`, // Replace with your third-party API base URL
+  baseURL: 'https://v3.football.api-sports.io', // Replace with your third-party API base URL
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization': `Bearer f22a9d427bce2ffa69a33c7db27f8f3e`, // Replace with your actual API key
+//   }
   headers: {
-    'x-rapidapi-host': process.env.FOOTBALL_API_HOST,
-    'x-rapidapi-key': process.env.FOOTBALL_API_KEY
+    'x-rapidapi-host': 'v3.football.api-sports.io',
+    'x-rapidapi-key': 'f22a9d427bce2ffa69a33c7db27f8f3e'
   }
 });
 
